ce36161d7321f46054a0ece3698605c36a0fa3de
gumingWu <15626185054@163.com>
2021-10-10 00:20:07 +0800

    prepare compiler to make the template change to render function

--- core/compiler/index.js
+++ core/compiler/index.js
@@ -1,4 +1,6 @@
 import Vue from '../instance/index'
+import { parse } from './parse'
+import { generate } from './codegen'
 
 // 源码的位置在entry-runtime-with-compiler.js，和runtime-only版本做区分
 Vue.prototype.$mount = function(el) {
@@ -32,7 +34,22 @@ function getOuterHTML(el) {
   }
 }
 
-// 将template变成render
+// 将template变成render函数
 function compileToFunctions(template) {
+  // 1. 将template变成ast语法树
+  // ast用来描述代码本身形成树结构，不仅可以描述html，也能描述css以及js语法
+  // 很多库都运用了ast，比如webpack，babel，eslint等
+  let ast = parse(template)
 
+  // 2. 优化静态节点，这里没实现
+  // if (options.optimize !== false) {
+  //   optimize(ast, options)
+  // }
+
+  // 3. 通过ast重新生成代码
+  // 类似_c('div',{id:"app"},_c('div',undefined,_v("hello"+_s(name)),_c('span',undefined,_v("world"))))
+  // _c代表创建元素 _v代表创建文本 _s代表文Json.stringify--把对象解析成文本
+  let code = generate(ast)
+  let render = new Function(`with(this){return ${code}}`)
+  return render
 }
